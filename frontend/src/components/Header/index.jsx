dcc7e2be8de8e5dd262ad488fd9df0b797411354
Hydrazl <joteixeirasimoes@gmail.com>
2025-11-03 17:55:51 -0300

    Modificações no Signin e Autenticação

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,7 +1,50 @@
-import './Header.css'
-import { Link } from 'react-router-dom'
+import './Header.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { FaAngleDown } from 'react-icons/fa';
+import { doLogout, getUser, isLogged } from '../../lib/AuthHandler';
+import { useAuth } from '../../contexts/AuthContext';
+
 
 export default function Header() {
+    const {logged, user, setLogged, setUser} = useAuth();
+    const [menuOpen, setMenuOpen] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const checkLogin = () => {
+            const loggedIn = isLogged();
+            setLogged(loggedIn);
+            if (loggedIn && !user) {
+                const userData = getUser();
+                setUser(userData);
+            } else {
+                setUser(null);
+            }
+        }
+
+        checkLogin()
+
+        const handleStorageChange = (e) => {
+            if (e.key === 'token' || e.key === 'user') {
+                checkLogin();
+            }
+        }
+
+        window.addEventListener('storage', handleStorageChange);
+
+        return () => {
+            window.removeEventListener('storage', handleStorageChange);
+        }
+    }, [setLogged, setUser]);
+
+    const handleLogout = () => {
+        doLogout();
+        setLogged(false);
+        setUser(null);
+        navigate('/signin');
+    }
+
     return (
         <header className="header">
             {/* logo */}
@@ -26,15 +69,38 @@ export default function Header() {
             {/* Botões */}
             <nav className="navbar">
                 <ul>
-                    <li>
-                        <Link to='/signin' className='entrarBtn'>Entrar</Link>
-                    </li>
-                    <li>
-                        <Link to='/' className='anunciarBtn'>Anunciar grátis</Link>
-                    </li>
+                    { logged ? (
+                        <>
+                            <li>
+                                <Link to='/' className='anuncio'>Meus Anúncios</Link>
+                            </li>
+                            <li>
+                                <Link to='/post-an-add' className='anunciarBtn'>Postar um anúncio</Link>
+                            </li>
+                            <li className="profileContainer">
+                                <button className='profileBtn' onClick={() => setMenuOpen(!menuOpen)}>
+                                    <img src={user.photoURL} alt={user.name} className='profileImg' />
+                                    <span>{user.name?.split(' ')[0]}</span><FaAngleDown size={16} className='arrow'/>
+                                </button>
+                                {menuOpen && (<div className='dropdown'>
+                                    <button>Minha Conta</button>
+                                    <button>Favoritos</button>
+                                    <button onClick={handleLogout}>Sair</button>
+                                </div>)}
+                            </li>
+                        </>
+                    ) : (
+                        <>
+                            <li>
+                                <Link to='/signin' className='entrarBtn'>Entrar</Link>
+                            </li>
+                            <li>
+                                <Link to='/' className='anunciarBtn'>Anunciar grátis</Link>
+                            </li>
+                        </>
+                    ) }
                 </ul>
             </nav>
-
         </header>
     )
 }

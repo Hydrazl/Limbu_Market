e63fa202952a092e9348872215de3509d181bc3c
Hydrazl <joteixeirasimoes@gmail.com>
2025-11-10 17:49:52 -0300

    Cards e Categorias criadas

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,10 +1,16 @@
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import './Home.css';
+import Categories from '../../components/Categorias'
+import Card from '../../components/Cards';
 
 export default function Page() {
     return (
         <div>
-            <h2>Página Home</h2>
-            <Link to='/about'>Home</Link>
+            <Categories/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
         </div>
     )
 }
